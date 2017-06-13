60146f53fa1df7685bf848a8cc80f926bf4f5502
Whaley-PC\Whaley <pt_yc@163.com>
2017-06-13 15:33:06 +0800

    new

--- dataquery/dataquery/settings/settings.py
+++ dataquery/dataquery/settings/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dataquery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dataquery',
+        'USER': 'miles',
+        'PASSWORD': 'moretvsmarTV@608_810',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
